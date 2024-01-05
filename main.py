2d90066c511a0624dc8d3244240d5a03a6024f39
LadaTich <ladislavtichna@gmail.com>
2024-01-05 22:26:41 +0100

    the basis created

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
-a_row = ["|", "|"]
-b_row = ["|", "|"]
-c_row = ["|", "|"]
+# a = [*"| | | |"]
+# b = [*"| | | |"]
+# c = [*"| | | |"]
 
-X_input = input("Player: X\nType a coordinates:  ")
-O_input = input("Player: O\nType a coordinates:  ")
+game_area = {
+    "a" : [*"| | | |"],
+    "b" : [*"| | | |"],
+    "c" : [*"| | | |"]
+}
+# 0,4,8
+end = False
+# putting into ui
 
+def is_it_end():
+    if game_area["a"][0] == game_area["a"][1] == game_area["a"][2]:
+        if game_area["a"][0] == "X":
+            print("\nX wins!\n")
 
+while end == False:
 
+    user_coordinates = [*input("Player: X\nType a coordinates:  ").lower()]
+
+    user_row = user_coordinates[0]
+    user_column = int(user_coordinates[1])
+
+    if user_column == 0:
+        user_column = 1
+    elif user_column == 1:
+        user_column = 3
+    elif user_column == 2:
+        user_column = 5
+
+    mark = "X"
+
+    affected_row = game_area[user_row]
+    affected_row[user_column] = mark
+
+    print(f'{" ".join(game_area["a"])}\n{" ".join(game_area["b"])}\n{" ".join(game_area["c"])}')
